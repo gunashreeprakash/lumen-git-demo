99dd48d0109f21f3a966a115d3c929c8fa639212
Gunashree P <gunashree.p@lumen.com>
2024-01-10 14:28:36 +0530

    manual merge

--- src/main/java/com/example/demo/apis/SampleApi.java
+++ src/main/java/com/example/demo/apis/SampleApi.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class SampleApi {
     @GetMapping
     public ResponseEntity<String> get(){
-        return ResponseEntity.ok("hello, master");
+        return ResponseEntity.ok("something different");
     }
 }
